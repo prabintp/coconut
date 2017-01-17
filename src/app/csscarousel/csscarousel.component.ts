75a708b0969bedfaa9e07ef8f9f376641536bdb3
prabin tp <prabintp@gmail.com>
2017-01-17 17:42:19 +0530

    carosal changes

--- src/app/csscarousel/csscarousel.component.ts
+++ src/app/csscarousel/csscarousel.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 //Import the Image interface
-import {Image} from './image.interface';
+//import {Image} from './image.interface';
 
 @Component({
   selector: 'app-csscarousel',
@@ -16,7 +16,7 @@ ngOnInit() {
   //images data to be bound to the template
   	public images = IMAGES;
     //IMAGES array implementing Image interface
-    var IMAGES: Image[] = [
+    IMAGES = [
     { "title": "Hire Cabs for Outstation...", "url": "app/csscarousel/images/nh47-Car-Hire.jpg" },
       { "title": "Hire Cabs for Outstation...", "url": "app/csscarousel/images/athirapalli-waterfall-package.jpg" },
         { "title": "Hire Cabs for Outstation...", "url": "app/csscarousel/images/nh47-Car-Hire.jpg" },
